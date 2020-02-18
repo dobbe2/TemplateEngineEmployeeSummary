2ee66fdad233cfb6ac292d4b4c6bf7e90c308689
James Dobbe <dobbe2@gmail.com>
2020-02-17 21:23:19 -0800

    basic structure complete.  Classes created.

---
+++ app.js
@@ -0,0 +1,22 @@
+///
+
+//import the classes
+
+
+//inquiere array object create  y a class
+
+var manager = new Manager(name,id,email,officeNumber)
+ array.push(manager)
+
+
+
+ for(var i=0; i< array.length; i++){
+
+    var role = array[i].getRole()
+
+    //swithch call templates based on the role
+    card = generateinter(data)
+    html = html + card
+ }
+
+ // fs your html
